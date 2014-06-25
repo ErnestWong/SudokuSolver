c0c7458d08fd53782e48cbc3325982fd3b3bb6a6
ErnestWong <ewong2222@hotmail.com>
2014-06-25 16:53:05 -0400

    class that accesses tess-two API

---
+++ src/com/example/sudokusolver/TessOCR.java
@@ -0,0 +1,32 @@
+public class TessOCR{
+    public static final String DATA_PATH;
+    
+    boolean trainTess(){
+        
+    }
+    void initOCR(){
+        int[][] grid = new int[9][9];
+        TessBaseAPI tessOCR = new TessBaseAPI();
+        
+        //datapath is in parent directory of tessdata
+        tessOCR.init(DATA_PATH, "eng");
+        tessOCR.setImage(bmp);
+        float xInc = bmp.getWidth() / 9;
+        float yInc = bmp.getHeight() / 9;
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                int x = xInc * i;
+                int y = xInc * j;
+                tessOCR.setRectangle(x, y, x + xInc, y + yInc);
+                String result = tessOCR.getUTF8Text();
+                if(result == null && result == "") {
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = Integer.parseInt(result);
+                }
+            }
+        }
+        tessOCR.end();
+    }
+}
