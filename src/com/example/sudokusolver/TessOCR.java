61afc6d8a8acc61d125ec0e1b164d60438b39198
ErnestWong <ewong2222@hotmail.com>
2014-07-11 00:56:33 -0400

    added tess-two library and tessdata files to project

--- src/com/example/sudokusolver/TessOCR.java
+++ src/com/example/sudokusolver/TessOCR.java
@@ -1,5 +1,7 @@
 package com.example.sudokusolver;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 public class TessOCR{
     //public static final String DATA_PATH;
     
@@ -7,29 +9,31 @@ public class TessOCR{
 		return false;
         
     }
-//    void initOCR(){
-//        int[][] grid = new int[9][9];
-//        TessBaseAPI tessOCR = new TessBaseAPI();
-//        
-//        //datapath is in parent directory of tessdata
-//        tessOCR.init(DATA_PATH, "eng");
-//        tessOCR.setImage(bmp);
-//        float xInc = bmp.getWidth() / 9;
-//        float yInc = bmp.getHeight() / 9;
-//        
-//        for(int i = 0; i < 9; i++){
-//            for(int j = 0; j < 9; j++){
-//                int x = xInc * i;
-//                int y = xInc * j;
-//                tessOCR.setRectangle(x, y, x + xInc, y + yInc);
-//                String result = tessOCR.getUTF8Text();
-//                if(result == null && result == "") {
-//                    grid[i][j] = 0;
-//                } else {
-//                    grid[i][j] = Integer.parseInt(result);
-//                }
-//            }
-//        }
-//        tessOCR.end();
-//    }
+    /*
+    void initOCR(){
+        int[][] grid = new int[9][9];
+        TessBaseAPI tessOCR = new TessBaseAPI();
+        
+        //datapath is in parent directory of tessdata
+        tessOCR.init(DATA_PATH, "eng");
+        tessOCR.setImage(bmp);
+        int xInc = bmp.getWidth() / 9;
+        int yInc = bmp.getHeight() / 9;
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                int x = xInc * i;
+                int y = xInc * j;
+                tessOCR.setRectangle(x, y, x + xInc, y + yInc);
+                String result = tessOCR.getUTF8Text();
+                if(result == null && result == "") {
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = Integer.parseInt(result);
+                }
+            }
+        }
+        tessOCR.end();
+    }
+    */
 }
