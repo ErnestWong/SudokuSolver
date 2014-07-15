317b4b0687e3c20ac5ea043f3c1a5e5c8eeab439
ErnestWong <ewong2222@hotmail.com>
2014-07-15 17:01:33 -0400

    new changes

--- src/com/example/sudokusolver/BlobExtraction.java
+++ src/com/example/sudokusolver/BlobExtraction.java
@@ -7,20 +7,22 @@ public class BlobExtraction {
 
 	private Bitmap bmp;
 	private int[][]label;
-	
+    //holds the set number and the list of pixels in that set
+    private Set<Integer, List<Point>> sets;
+    
 	public BlobExtraction(Bitmap bitmap){
 		bmp = bitmap;
 		label = new int[bitmap.getWidth()][bitmap.getHeight()];
 	}
-	
+
 	public void blobExtract() {
 		int currentLabel = 1;
 
-		for (int i = 0; i < bmp.getWidth(); i++) {
-			for (int j = 0; j < bmp.getHeight(); j++) {
+		for (int i = 1; i < bmp.getWidth()-1; i++) {
+			for (int j = 1; j < bmp.getHeight()-1; j++) {
 				if (bmp.getPixel(i, j) == Color.WHITE) {
 					int num = checkNeighbors(i, j);
-					if (num <= 0) {
+					if (num == 0) {
 						label[i][j] = currentLabel;
 						currentLabel++;
 					} else {
@@ -36,7 +38,7 @@ public class BlobExtraction {
 		for (int i = x - 1; i <= x + 1; i++) {
 			for (int j = y - 1; j <= y + 1; j++) {
 				if (i != x && j != y && bmp.getPixel(i, j) == Color.WHITE) {
-					if (label[i][j] > 0) {
+					if (label[i][j] != 0) {
 						if (label[j][j] < min) {
 							min = label[i][j];
 						}
